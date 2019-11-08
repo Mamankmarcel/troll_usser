89179f2323e6ea54ac7178ee38171e496aaa3e8a
Mamankmarcel <57529908+Mamankmarcel@users.noreply.github.com>
2019-11-08 20:35:22 +0700

    troll_user.sh

---
+++ troll_user.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+clear
+echo
+echo "\033[1;33mTroll_user script"
+sleep 2
+echo
+echo
+echo "tunggu sebentar!!.. "
+echo
+echo
+sleep 2
+echo "script akan dijalankan"
+sleep 2
+cd /sdcard
+mkdir monyet
+mkdir anjing
+mkdir paus
+mkdir kelinci
+mkdir banteng
+sleep 2
+echo "mungkin saat ini anda berpikir apa kegunaannya script ini? "
+echo "kalo anda tau ini kegunaanya apa"
+echo "mungkin anda bisa suruh teman anda yg paling dibenci"
+echo "untuk menggunakan ini"
+sleep 2
+echo "script ini tidak terlalu berbahaya"
+echo 
+echo
+echo "kalau ingin tau kegunaan script ini"
+echo "anda bisa liat file manager anda! "
+sleep 5
+exit
